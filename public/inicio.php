5d1fb5df2ebc85212b1565d06452f6367cae49a4
llerandiortizd <124943226+llerandiortizd@users.noreply.github.com>
2023-02-28 19:03:07 +0100

    Create inicio.php

---
+++ public/inicio.php
@@ -0,0 +1,25 @@
+<?php require_once("../resources/config.php"); ?>
+<?php require_once("../resources/auth_check.php");?>
+
+<?php include(TEMPLATE_FRONT . DS . "header.php"); ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">            
+
+                <div class="row carousel-holder">
+
+                    <div class="col-md-12">
+                        
+						<?php include(TEMPLATE_FRONT . DS . "slider.php"); ?>
+						
+                    </div>
+
+                </div>
+
+        </div>
+
+    </div>
+    <!-- /.container -->
+	<?php include(TEMPLATE_FRONT . DS . "footer.php"); ?>
